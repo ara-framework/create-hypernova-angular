4435c12ccabb416767d12fa7c1e1cbe64629bab2
Felipe Guizar Diaz <felipegaiacharly@gmail.com>
2019-11-10 16:06:03 -0600

    feat: support nova mount event in entry point for browser

--- template/src/browser.main.ts
+++ template/src/browser.main.ts
@@ -1,9 +1,26 @@
 import 'core-js/proposals/reflect-metadata';
 import 'zone.js';
 
-import { renderAngular } from 'hypernova-angular'
+import { load, mountComponent, loadById } from 'hypernova-angular'
 
 import { ExampleModule } from './components/example/example.module'
 import { ExampleComponent } from './components/example/example.component'
 
-renderAngular('Example', ExampleComponent, ExampleModule)
+const render = (name, { node, data }) => {
+  if (name === 'Example') {
+    return mountComponent(ExampleComponent, ExampleModule, node, data)
+  }
+}
+
+document.addEventListener('NovaMount', (event) => {
+  const { name, id } = (<CustomEvent>event).detail
+
+  const payload = loadById(name, id)
+
+  if (payload) {
+    render(name, payload)
+  }
+})
+
+
+load('Example').forEach(render.bind(null, 'Example'))
